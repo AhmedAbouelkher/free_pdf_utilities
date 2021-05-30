07881f67054980dd2ef7ca27f23a828818f007b5
AhmedAbouelkher <am303737@gmail.com>
2021-05-30 13:32:48 +0200

    add appsettings

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,34 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
+
 import 'package:free_pdf_utilities/Modules/Common/Utils/constants.dart';
 import 'package:free_pdf_utilities/Modules/Common/Utils/shared_prefs_utils.dart';
+import 'package:free_pdf_utilities/Modules/Settings/Models/app_settings.dart';
+import 'package:free_pdf_utilities/Modules/Settings/settings_service.dart';
+import 'package:free_pdf_utilities/Screens/error_db_screen.dart';
 import 'package:free_pdf_utilities/Screens/root_screen.dart';
-import 'package:window_size/window_size.dart';
+
+import 'Modules/Settings/settings_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PreferenceUtils.init();
+  Hive.registerAdapter(PdfPageFormatEnumAdapter());
+  Hive.registerAdapter(PageOrientationEnumAdapter());
+  Hive.registerAdapter(AppSettingsAdapter());
+  Hive.registerAdapter(PDFExportOptionsAdapter());
+  await Hive.initFlutter();
+
+  await SettingService.init();
+
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     setWindowTitle("");
-    setWindowMinSize(const Size(800, 500));
+    setWindowMinSize(const Size(600, 500));
     setWindowMaxSize(Size.infinite);
   }
 
@@ -42,29 +59,55 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: kAppName,
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      home: const RootScreen(),
-      theme: ThemeData.dark().copyWith(
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.cyan,
-          foregroundColor: Colors.white,
-        ),
-        textButtonTheme: _textButtonThemeData,
-        dialogTheme: DialogTheme(
-          shape: _roundedShape,
-          backgroundColor: ThemeData.dark().scaffoldBackgroundColor,
-          titleTextStyle: ThemeData.dark().textTheme.headline1,
-          contentTextStyle: ThemeData.dark().textTheme.bodyText1,
-        ),
-        appBarTheme: AppBarTheme(
-          elevation: 0.0,
-          iconTheme: IconThemeData(
-            size: 18,
-          ),
-        ),
+    var _settingBox = SettingService.box;
+    if (_settingBox == null) {
+      return ErrorDBScreen();
+    }
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AppSettingsProvider()),
+      ],
+      child: ValueListenableBuilder(
+        valueListenable: _settingBox.listenable(),
+        builder: (context, box, _) {
+          final _appSettings = SettingService.read();
+          return MaterialApp(
+            title: kAppName,
+            debugShowCheckedModeBanner: false,
+            themeMode: SettingsThemeMode.getThemeMode(_appSettings.themeMode!),
+            home: const RootScreen(),
+            darkTheme: ThemeData.dark().copyWith(
+              scaffoldBackgroundColor: const Color(0xFF1D1E1F),
+              outlinedButtonTheme: OutlinedButtonThemeData(
+                style: OutlinedButton.styleFrom(
+                  primary: Colors.grey,
+                  backgroundColor: Colors.grey[850],
+                  side: BorderSide(
+                    color: Colors.grey[800]!,
+                  ),
+                ),
+              ),
+              floatingActionButtonTheme: FloatingActionButtonThemeData(
+                backgroundColor: Colors.cyan,
+                foregroundColor: Colors.white,
+              ),
+              textButtonTheme: _textButtonThemeData,
+              dialogTheme: DialogTheme(
+                shape: _roundedShape,
+                backgroundColor: ThemeData.dark().scaffoldBackgroundColor,
+                titleTextStyle: ThemeData.dark().textTheme.headline1,
+                contentTextStyle: ThemeData.dark().textTheme.bodyText1,
+              ),
+              appBarTheme: AppBarTheme(
+                elevation: 0.0,
+                iconTheme: IconThemeData(
+                  size: 18,
+                ),
+              ),
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+          );
+        },
       ),
     );
   }
